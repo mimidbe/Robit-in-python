c54e4e9a99356c18a52840ffa39c1332e35aadba
mimidbe <51967427+mimidbe@users.noreply.github.com>
2020-11-09 20:38:21 +0100

    Update Robit.py

--- Robit.py
+++ Robit.py
@@ -151,7 +151,7 @@ class Robit:
         utime.sleep_us(10)
         pin.write_digital(0)
         # Get the duration, in microseconds, of a pulse high from one of the pin
-        distance = machine.time_pulse_us(pin, 1,23000) / 58
+        distance = machine.time_pulse_us(pin, 1, 23000) / 58
         return distance
         
     def init_line_follow(self, jpin):
