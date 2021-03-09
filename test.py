8e53a61a0a38332bc408222024e295c73f27d13f
mimidbe <51967427+mimidbe@users.noreply.github.com>
2021-03-09 19:47:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 from microbit import *
-import Robit
-from Robit import J1
+from Robit import *
 
 item = 0
 Mbit = Robit() 
-Mbit.init_line_follow(J1)
+Mbit.init_line_follow("J1")
 
 while True:
     if Mbit.left_line_follow() == 1 and Mbit.right_line_follow() == 0:
